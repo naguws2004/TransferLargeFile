86bf45c19a638029958678a835f7645b4ea779e9
Nagesh Kumar Y <nageshkumar.y@gmail.com>
2024-12-11 17:32:15 +0530

    commit

---
+++ DataFileUploader/DataFileUploader/DataFile.cs
@@ -0,0 +1,48 @@
+﻿namespace DataFileUploader
+{
+    internal class DataFile
+    {
+        private readonly static string _startupPath = Directory.GetCurrentDirectory();
+
+        public static bool CreateFile(string tempFileName, int count)
+        {
+            try
+            {
+                // Create a file path within the data directory
+                string filePath = Path.Combine(_startupPath, "data", tempFileName);
+
+                // Create the file and write some text
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    string jsonFilePath = Path.Combine(_startupPath, "sample.json");
+                    // Check if the file exists
+                    if (File.Exists(jsonFilePath))
+                    {
+                        // Read the text from the file
+                        string text = File.ReadAllText(filePath);
+                        for (int i = 0; i < count; i++)
+                        {
+                            // Print the text to the file
+                            writer.WriteLine(text);
+                            writer.WriteLine(Environment.NewLine);
+                        }
+                    }
+                }
+
+                Console.WriteLine("File created at: " + filePath);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static void RenameFile(string tempFileName, string dataFileName)
+        {
+            string filePath = Path.Combine(_startupPath, "data", tempFileName);
+            string dataFilePath = Path.Combine(_startupPath, "data", dataFileName);
+            File.Move(filePath, dataFilePath);
+        }
+    }
+}
---
+++ DataFileUploader/DataFileUploader/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace DataFileUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var tempFileName = "tempFile.dat";
+            //var dataFileName = "dataFile.dat";
+            //var created = DataFile.CreateFile(tempFileName, 10);
+            //if (created) DataFile.RenameFile(tempFileName, dataFileName);
+            while (true) { }
+            
+        }
+    }
+}
